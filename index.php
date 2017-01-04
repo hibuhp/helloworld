09bf19191b9d78cb65ca5d279d4835a5db795341
HP-BOOK\HP <hp@essenzwissen.at>
2017-01-04 19:11:25 +0100

    hello world

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>hello world</title>
+</head>
+<body>
+	<h1>Hello World</h1>
+	<script>
+		alert('hello World');
+	</script>
+	<?php  
+		echo "<h2>Hello PHP</h2>";
+	?>
+</body>
+</html>
